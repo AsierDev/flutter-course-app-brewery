26c6a5c4d709da7cd0a1e84e459395fbd6f7c744
Asier <29598090+AsierDev@users.noreply.github.com>
2020-06-01 14:38:43 +0200

    complete xylophone

--- xylophone-flutter/lib/main.dart
+++ xylophone-flutter/lib/main.dart
@@ -9,20 +9,46 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              color: Colors.deepPurple,
-              padding: EdgeInsets.all(12.0),
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click me'),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: addXylophoneKeys(),
           ),
         ),
       ),
     );
   }
+}
+
+class Note {
+  var keyColor;
+  String noteSound;
+
+  Note(this.keyColor, this.noteSound);
+}
+
+List<Widget> addXylophoneKeys() {
+  List<Note> notes = List<Note>();
+  notes.add(Note(Colors.blueGrey, 'note1.wav'));
+  notes.add(Note(Colors.indigo, 'note2.wav'));
+  notes.add(Note(Colors.teal[700], 'note3.wav'));
+  notes.add(Note(Colors.lightGreen, 'note4.wav'));
+  notes.add(Note(Colors.amber, 'note5.wav'));
+  notes.add(Note(Colors.orange, 'note6.wav'));
+  notes.add(Note(Colors.red[600], 'note7.wav'));
+
+  return notes
+      .map(
+        (element) => Expanded(
+          child: FlatButton(
+            color: element.keyColor,
+            onPressed: () {
+              final player = AudioCache();
+              player.play(element.noteSound);
+            },
+          ),
+        ),
+      )
+      .toList();
 }
