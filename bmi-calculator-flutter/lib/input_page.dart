a913bf7bf10a31453c4bba41816c48e7e5aa632a
Asier Perez <29598090+AsierDev@users.noreply.github.com>
2020-07-15 21:51:30 +0200

    add containers

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const activeCardColor =  Color(0xFF1D1E33);
+const buttonColor =  Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -12,9 +15,64 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body:Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Cards(color: activeCardColor),
+                ),
+                Expanded(
+                  child: Cards(color: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Cards(color: activeCardColor),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Cards(color: activeCardColor),
+                ),
+                Expanded(
+                  child: Cards(color: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10.0),
+            decoration: BoxDecoration(
+              color: buttonColor,
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+            height: 80.0,
+            width: double.infinity,
+          ),
+        ],
       ),
     );
   }
 }
+
+class Cards extends StatelessWidget {
+  Cards({@required this.color});
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
+
