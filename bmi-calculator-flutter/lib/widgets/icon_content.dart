7de9776877277e316f545cf0421d2e669ebae0cc
Asier Perez <29598090+AsierDev@users.noreply.github.com>
2020-07-17 14:00:03 +0200

    add folder structure

--- bmi-calculator-flutter/lib/widgets/icon_content.dart
+++ bmi-calculator-flutter/lib/widgets/icon_content.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-import 'constants.dart';
+import '../utils/constants.dart';
 
 class IconContent extends StatelessWidget {
   IconContent({@required this.icon, @required this.label});
