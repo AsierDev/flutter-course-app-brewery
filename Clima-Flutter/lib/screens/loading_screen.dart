9f1b1f6bb284a67a05763976c109437ef2952b74
Asier <29598090+AsierDev@users.noreply.github.com>
2020-08-19 18:20:33 +0200

    add weather api

--- Clima-Flutter/lib/screens/loading_screen.dart
+++ Clima-Flutter/lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,8 +21,16 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'latitude =>  ${location.latitude}, longitude => ${location.longitude}');
   }
 
+  void getData() async {
+    http.Response response = await http.get(
+        'http://samples.openweathermap.org/data/2.5/weather?lat=41.38879&lon=2.15899&appid=6fe6cdd20b7c721a7788c0222ad8b638');
+
+    print(' response => ${response.statusCode}');
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold();
   }
 }
