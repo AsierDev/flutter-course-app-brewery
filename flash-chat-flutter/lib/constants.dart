6e410c85d24b217a8eb30accaa3501699c73da61
Asier <29598090+AsierDev@users.noreply.github.com>
2020-08-24 13:12:59 +0200

    refactor and finetune styles

--- flash-chat-flutter/lib/constants.dart
+++ flash-chat-flutter/lib/constants.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+// Default texts
+String kLogo = 'logo';
+
+//Styles
+
 const kSendButtonTextStyle = TextStyle(
   color: Colors.lightBlueAccent,
   fontWeight: FontWeight.bold,
@@ -18,4 +23,18 @@ const kMessageContainerDecoration = BoxDecoration(
   ),
 );
 
-String kLogo = 'logo';
+const kTextFieldDecoration = InputDecoration(
+  hintText: 'Enter a value',
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
