493e162169877cd1554f0fc0e3d12dc6863d323e
Asier <29598090+AsierDev@users.noreply.github.com>
2020-08-28 19:18:56 +0200

    add tasks screen design

--- flutter_todo/lib/screens/tasks_screen.dart
+++ flutter_todo/lib/screens/tasks_screen.dart
@@ -1,8 +1,70 @@
 import 'package:flutter/material.dart';
+import '../utils/constants.dart';
 
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: kMainColor,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: kMainColor,
+        child: Icon(
+          Icons.add,
+          size: 30.0,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, right: 30.0, bottom: 30.0, left: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 30.0,
+                    color: kMainColor,
+                  ),
+                  backgroundColor: kSecondaryColor,
+                  radius: 30.0,
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todo App',
+                  style: TextStyle(
+                    color: kSecondaryColor,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 tasks',
+                  style: TextStyle(
+                    color: kSecondaryColor,
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: kSecondaryColor,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20.0),
+                  topLeft: Radius.circular(20.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ flutter_todo/lib/utils/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const kMainColor = Colors.lightBlueAccent;
+const kSecondaryColor = Colors.white;
